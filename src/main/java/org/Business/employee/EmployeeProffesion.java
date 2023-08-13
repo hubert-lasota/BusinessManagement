f8c5275e61deb41c6fed81978b5f4bcb6a54d11c
Hubert L <random@email.com>
2023-08-13 20:27:55 +0200

    create enum EmployeeProffesion

---
+++ src/main/java/org/Business/employee/EmployeeProffesion.java
@@ -0,0 +1,15 @@
+package org.Business.employee;
+
+public enum EmployeeProffesion {
+    CEO,
+    VICE_CEO,
+    DIRECTOR,
+    MANAGER,
+    RECRUITER,
+    SOCIAL_MEDIA_SPECIALIST,
+    UI_UX_DESIGNER,
+    FRONTEND_DEVELOPER,
+    BACKEND_DEVELOPER,
+    DEVOPS,
+    NONE
+}
