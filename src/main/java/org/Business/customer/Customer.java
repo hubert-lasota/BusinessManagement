cb4d3fd52f99c16d0c24b2c8daee1d1da13691be
Hubert L <random@email.com>
2023-08-24 17:51:16 +0200

    add address to Customer

--- src/main/java/org/Business/customer/Customer.java
+++ src/main/java/org/Business/customer/Customer.java
@@ -5,13 +5,66 @@ public class Customer {
     private static long incrementID = 1;
 
     public String name;
+    public Address address;
 
-    public Customer(String name) {
+    public Customer(String name, Address address) {
         this.name = name;
+        this.address = address;
         ID = incrementID++;
+    }
+
+    public static class Address {
+        private String streetWithNumber;
+        private String postalCode;
+        private String city;
+        private String country;
+
+        public Address(String streetWithNumber, String postalCode, String city, String country) {
+            this.streetWithNumber = streetWithNumber;
+            this.postalCode = postalCode;
+            this.city = city;
+            this.country = country;
+        }
+
+        public String getStreetWithNumber() {
+            return streetWithNumber;
+        }
+
+        public void setStreetWithNumber(String streetWithNumber) {
+            this.streetWithNumber = streetWithNumber;
+        }
+
+        public String getPostalCode() {
+            return postalCode;
+        }
+
+        public void setPostalCode(String postalCode) {
+            this.postalCode = postalCode;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+        public String getCountry() {
+            return country;
+        }
+
+        public void setCountry(String country) {
+            this.country = country;
+        }
+
     }
 
     public long getID() {
         return ID;
+    }
+
+    public String getName() {
+        return name;
     }
 }
