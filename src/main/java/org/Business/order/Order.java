94808ca5450e4729e2a681154f0cf14f2b44455f
Hubert L <random@email.com>
2023-08-28 11:12:36 +0200

    Order: change products List to Map add field totalPrice

--- src/main/java/org/Business/order/Order.java
+++ src/main/java/org/Business/order/Order.java
@@ -3,19 +3,21 @@ package org.Business.order;
 import org.Business.customer.Customer;
 import org.Business.product.Product;
 
-import java.util.List;
+import java.util.Map;
 
 public class Order {
     private final long ID;
     private static long incrementID = 1;
 
     private final Customer customer;
-    private final List<Product> products;
+    private final Map<Product, Integer> products;
+    private final double totalPrice;
     private String orderComments;
 
-    public Order(Customer customer, List<Product> products) {
+    public Order(Customer customer, Map<Product, Integer> products) {
         this.customer = customer;
         this.products = products;
+        this.totalPrice = countTotalPrice(products);
         ID = incrementID++;
     }
 
@@ -28,11 +30,39 @@ public class Order {
     }
 
     public void addProduct(Product product) {
-        products.add(product);
+        products.put(product, products.get(product) + 1);
     }
 
     public void removeProduct(Product product) {
         products.remove(product);
+    }
+
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public long getID() {
+        return ID;
+    }
+
+
+    public static double countTotalPrice(Map<Product, Integer> order) {
+        double result = 0.0;
+        for (Map.Entry<Product, Integer> product: order.entrySet()) {
+            result += product.getValue() * product.getKey().getPrice();
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        String result = "Order: " + ID + ".\n" + customer + "\n";
+        StringBuilder tempStrBuilder = new StringBuilder();
+        for(Map.Entry<Product, Integer> product : products.entrySet()) {
+            tempStrBuilder.append("Quantity: ").append(product.getValue()).append("\n").append(product.getKey()).append("\n");
+        }
+        return result += tempStrBuilder.toString();
     }
 
 
