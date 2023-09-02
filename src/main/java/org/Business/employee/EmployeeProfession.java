2a70b5274881ececf07a30339cf3e93498d4f96d
Hubert L <random@email.com>
2023-09-02 19:43:34 +0200

    EmployeeProfession: remove unnecessary fields

--- src/main/java/org/Business/employee/EmployeeProfession.java
+++ src/main/java/org/Business/employee/EmployeeProfession.java
@@ -1,16 +1,8 @@
 package org.Business.employee;
 
 public enum EmployeeProfession {
-    CEO,
-    VICE_CEO,
-    DIRECTOR,
-    MANAGER,
-    RECRUITER,
-    SOCIAL_MEDIA_SPECIALIST,
-    UI_UX_DESIGNER,
-    FRONTEND_DEVELOPER,
-    BACKEND_DEVELOPER,
-    DEVOPS,
-    ADMIN,
+    DEVELOPER,
+    WAREHOUSE_WORKER,
+    ADMINISTRATOR,
     NONE
 }
