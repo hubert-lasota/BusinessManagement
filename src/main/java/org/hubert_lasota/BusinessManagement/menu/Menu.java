8ec07ff54c5f6927f40dfd0c8bd6ec05be62c356
hubert-lasota <hxprogramming@gmail.com>
2024-01-08 17:55:45 +0100

    - delete and stop track

--- src/main/java/org/hubert_lasota/BusinessManagement/menu/Menu.java
+++
@@ -1,7 +0,0 @@
-package org.hubert_lasota.BusinessManagement.menu;
-
-public interface Menu {
-
-    void generateMenu();
-
-}
--- src/main/java/org/hubert_lasota/BusinessManagement/order/OrderStatus.java
+++
@@ -1,7 +0,0 @@
-package org.hubert_lasota.BusinessManagement.order;
-
-public enum OrderStatus {
-    NEW,
-    IN_PROGRESS,
-    COMPLETED
-}
